97756d36ecd26c1041f7fb141a10e3aa39c37253
alex_adusei1 <alex.a.adusei@gmail.com>
2019-12-10 23:33:01 +0000

    added mutators to models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,5 +4,15 @@ namespace App\Models;
 
 class Category extends ApiModel
 {
-    //
+    protected $fillable = ['code', 'title'];
+
+    public function setCodeAttribute($value)
+    {
+        $this->attributes['code'] = \ucwords($value);
+    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = ucfirst($value);
+    }
 }
--- app/Models/Code.php
+++ app/Models/Code.php
@@ -4,5 +4,30 @@ namespace App\Models;
 
 class Code extends ApiModel
 {
-    //
+    protected $fillable = ['category_id', 'diagnosis_code', 'full_code', 'abbreviated_description', 'full_description'];
+
+    public function setDiagnosisCodeAttribute($value)
+    {
+        $this->attributes['diagnosis_code'] = \ucwords($value);
+    }
+
+    public function setFullCodeAttribute($value)
+    {
+        $this->attributes['full_code'] = \ucwords($value);
+    }
+
+    public function setAbbreviatedDescriptionAttribute($value)
+    {
+        $this->attributes['abbreviated_description'] = ucfirst($value);
+    }
+
+    public function setFullDescriptionAttribute($value)
+    {
+        $this->attributes['full_description'] = ucfirst($value);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
