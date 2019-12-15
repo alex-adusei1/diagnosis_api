8754f1d6977e644959f1ae967fad930f6bd4f6c5
alex_adusei1 <alex.a.adusei@gmail.com>
2019-12-15 12:42:58 +0000

    added postman collection and included seeder data for catgeory

--- database/seeds/CategoryTableSeeder.php
+++ database/seeds/CategoryTableSeeder.php
@@ -20,8 +20,40 @@ class CategoryTableSeeder extends Seeder
             ),
 
             1 => array(
+                'code' => 'A01',
+                'title' => 'Typhoid and paratyphoid fever',
+            ),
+            2 => array(
                 'code' => 'A010',
                 'title' => 'Typhoid fever',
+            ),
+            3 => array(
+                'code' => 'A011',
+                'title' => 'Paratyphoid fever A',
+            ),
+            4 => array(
+                'code' => 'A012',
+                'title' => 'Paratyphoid fever B',
+            ),
+            5 => array(
+                'code' => 'A013',
+                'title' => 'Paratyphoid fever C',
+            ),
+            6 => array(
+                'code' => 'A014',
+                'title' => 'Paratyphoid fever, unspecified',
+            ),
+            7 => array(
+                'code' => 'A02',
+                'title' => 'Other salmonella infection',
+            ),
+            8 => array(
+                'code' => 'A022',
+                'title' => 'Localized salmonella infections',
+            ),
+            9 => array(
+                'code' => 'A028',
+                'title' => 'Other specified salmonella infections',
             ),
         ));
     }
